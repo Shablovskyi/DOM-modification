049dd0b97287d6f5bc103d68c9a1c029f5bbe891
Shablovskyi <shablovskyiillia@gmail.com>
2024-07-03 09:08:00 +0300

    fix func addElementBtn

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-const addElementBtn = document.getElementById("addElementBtn")
-const toggleClassBtn = document.getElementById("toggleClassBtn")
+const addElementBtn = document.getElementById("addElementBtn");
+const toggleClassBtn = document.getElementById("toggleClassBtn");
 
-const content = document.getElementById("content")
-
-
+const content = document.getElementById("content");
 
 addElementBtn.addEventListener("click", () => {
-  const createElem = document.createElement("p")
-  const elemText = document.createTextNode("New element")
-  createElem.appendChild(elemText)
+  const createElem = document.createElement("p");
+  createElem.innerText = "New element";
   content.appendChild(createElem);
-})
+});
 
 toggleClassBtn.addEventListener("click", () => {
-  const elements = content.querySelectorAll("p")
-  elements.forEach(elem => {
-    if(elem.classList.contains("highlight")){
-      elem.classList.remove("highlight")
-    }else{
-      elem.classList.add("highlight")
+  const elements = content.querySelectorAll("p");
+  elements.forEach((elem) => {
+    if (elem.classList.contains("highlight")) {
+      elem.classList.remove("highlight");
+    } else {
+      elem.classList.add("highlight");
     }
-
   });
-})
+});
