9eb3ce0e62814454e6800ec2c3ba4d58ef401c9a
Shablovskyi <shablovskyiillia@gmail.com>
2024-07-03 11:18:58 +0300

    create func on btn Insert Button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addElementBtn = document.getElementById("addElementBtn");
 const toggleClassBtn = document.getElementById("toggleClassBtn");
 const replaceElemBtn = document.getElementById("replaceElementBtn");
+const insertFragmentBtn = document.getElementById("insertFragmentBtn");
 
 const content = document.getElementById("content");
 
@@ -22,6 +23,21 @@ toggleClassBtn.addEventListener("click", () => {
 });
 
 replaceElemBtn.addEventListener("click", () => {
-  const elements = content.querySelectorAll("*")
+  const elements = content.querySelectorAll("p");
   elements[0].innerText = "New text"
+})
+
+insertFragmentBtn.addEventListener("click", () => {
+  const fragment = document.createDocumentFragment();
+
+  const newP1 = document.createElement("p")
+  newP1.innerText = "New paragraf 1"
+
+  const newP2 = document.createElement("p")
+  newP2.innerText = "New paragraf 2"
+
+  fragment.appendChild(newP1)
+  fragment.appendChild(newP2)
+  
+  content.prepend(fragment)
 })
