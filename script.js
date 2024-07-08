60d2ffa686a7f923e112a5747d4ade8dfd106395
Shablovskyi <shablovskyiillia@gmail.com>
2024-07-08 16:09:20 +0300

    fix reading

--- script.js
+++ script.js
@@ -8,22 +8,21 @@ const content = document.getElementById("content");
 addElementBtn.addEventListener("click", () => {
   const createElem = document.createElement("p");
   createElem.innerText = "New element";
-  createElem.classList.add("text-emerald-500")
+  createElem.classList.add("text-emerald-500");
   content.appendChild(createElem);
 });
 
-
 toggleClassBtn.addEventListener("click", (e) => {
   const elements = content.querySelectorAll("p");
-  elements.forEach(elements => {
-    elements.classList.toggle("highlight")
-  })
+  elements.forEach((elements) => {
+    elements.classList.toggle("highlight");
+  });
 });
 
 replaceElemBtn.addEventListener("click", () => {
   const elements = content.querySelectorAll("p");
   elements[0].innerText = "New text";
-  elements[0].classList.add("text-lime-500")
+  elements[0].classList.add("text-lime-500");
 });
 
 insertFragmentBtn.addEventListener("click", () => {
@@ -31,11 +30,11 @@ insertFragmentBtn.addEventListener("click", () => {
 
   const newP1 = document.createElement("p");
   newP1.innerText = "New paragraph 1";
-  newP1.classList.add("text-rose-500")
+  newP1.classList.add("text-rose-500");
 
   const newP2 = document.createElement("p");
   newP2.innerText = "New paragraph 2";
-  newP2.classList.add("text-rose-500")
+  newP2.classList.add("text-rose-500");
 
   fragment.appendChild(newP1);
   fragment.appendChild(newP2);
